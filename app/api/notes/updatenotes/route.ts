c90d9beec7b289f8b527a4ba7683c5da9aa36f8e
rutikakhedkar <rutikakhadekar@gmail.com>
2026-03-22 13:06:51 +0530

    changes

--- app/api/notes/updatenotes/route.ts
+++ app/api/notes/updatenotes/route.ts
@@ -2,25 +2,25 @@ import { NextResponse } from "next/server";
 import { prisma } from "@/lib/prisma";
 import { DeleteObjectCommand, S3Client } from "@aws-sdk/client-s3";
 
-const accessKeyId = process.env.AWS_ACCESS_KEY;
-const secretAccessKey = process.env.AWS_SECRET_KEY;
-
-if (!accessKeyId || !secretAccessKey) {
-  throw new Error("Missing AWS credentials");
-}
-
-const s3Client = new S3Client({
-  region: "ap-south-1",
-  credentials: {
-    accessKeyId,
-    secretAccessKey
-  }
-});
-
 export const deleteFileFromS3 = async (key: string) => {
+  const accessKeyId = process.env.AWS_ACCESS_KEY;
+  const secretAccessKey = process.env.AWS_SECRET_KEY;
+
+  if (!accessKeyId || !secretAccessKey) {
+    throw new Error("Missing AWS credentials");
+  }
+
+  const s3Client = new S3Client({
+    region: "ap-south-1",
+    credentials: {
+      accessKeyId,
+      secretAccessKey,
+    },
+  });
+
   await s3Client.send(
     new DeleteObjectCommand({
-      Bucket: 'amzn-notesapp-private',
+      Bucket: "amzn-notesapp-private",
       Key: key,
     })
   );
@@ -36,14 +36,11 @@ export async function PATCH(req: Request) {
     const summary = formData.get("summary") as string;
     const notesId = formData.get("notesId") as string;
 
-    //  parse removed files
     const removedFiles = JSON.parse(
       (formData.get("removedFiles") as string) || "[]"
     );
 
     const files = formData.getAll("file") as File[];
-
-    // ⚠️ Assume files already uploaded → names are keys
     const newFilePaths = files.map((f) => f.name);
 
     const existingNote = await prisma.notes.findUnique({
@@ -59,18 +56,15 @@ export async function PATCH(req: Request) {
 
     let updatedFilePaths = existingNote.filePaths || [];
 
-    //  remove deleted files from DB
     if (removedFiles.length > 0) {
       updatedFilePaths = updatedFilePaths.filter(
         (path: string) => !removedFiles.includes(path)
       );
     }
 
-    // add new files
     if (newFilePaths.length > 0) {
       updatedFilePaths = [...updatedFilePaths, ...newFilePaths];
     }
-
 
     const updateData: any = {};
     const parsedTags = JSON.parse(tags);
@@ -100,7 +94,6 @@ export async function PATCH(req: Request) {
       data: updateData,
     });
 
-    // ✅ delete from S3 AFTER DB update
     if (removedFiles.length > 0) {
       for (const key of removedFiles) {
         await deleteFileFromS3(key);
